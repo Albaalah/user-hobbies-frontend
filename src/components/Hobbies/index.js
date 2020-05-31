95a3f049ed59d468f323646290a61b56ec310959
Albaalah <albaalahwaqar@yahoo.com>
2020-05-31 10:45:18 +0500

    Delete hobby

--- src/components/Hobbies/index.js
+++ src/components/Hobbies/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import './index.scss';
 import {
     Button,
@@ -16,10 +16,41 @@ import {isEmpty} from 'lodash';
 const Hobbies = () => {
     const hobbiesDetail = useSelector(state => state.hobbies.hobbiesDetail);
     const loading = useSelector(state => state.users.loading);
+    const [hobbyInfo, setHobbyInfo] = React.useState([]);
 
     if (loading) {
         return <CircularProgress/>
     }
+
+    const onDelete = (id) => {
+        // store current data in state and manipulate component state to shallow remove
+        const data = !isEmpty(hobbyInfo) ? hobbyInfo : hobbiesDetail;
+        const {hobbies} = data;
+        hobbies.splice(hobbies.findIndex(item => item._id === id), 1);
+        setHobbyInfo({hobbies})
+    };
+
+    const renderHobbies = () => {
+        const HobbyList = !isEmpty(hobbyInfo) ? hobbyInfo.hobbies : hobbiesDetail.hobbies;
+        return <Fragment>
+            {
+                HobbyList.map((hobby, index) => {
+                    const {_id = '', title = '', passion = '', year = ''} = hobby;
+                    return <TableRow key={_id}>
+                        <TableCell>{index + 1}</TableCell>
+                        <TableCell component="th" scope="row">
+                            {title}
+                        </TableCell>
+                        <TableCell align="right">{passion}</TableCell>
+                        <TableCell align="right">{year}</TableCell>
+                        <TableCell align="right">
+                            <Button variant="outlined" color="primary" onClick={() => onDelete(_id)}>Delete</Button>
+                        </TableCell>
+                    </TableRow>
+                })
+            }
+        </Fragment>
+    };
 
     return <div className={"resize hobbies-container"}>
         <div className='d-flex justify-content-between m-2'>
@@ -35,20 +66,11 @@ const Hobbies = () => {
                                 <TableCell>Hobby</TableCell>
                                 <TableCell align="right">Passion</TableCell>
                                 <TableCell align="right">Year</TableCell>
+                                <TableCell align="right"/>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {hobbiesDetail.hobbies.map((hobby, index) => {
-                                const {_id = '', title = '', passion = '', year = ''} = hobby;
-                                return <TableRow key={_id}>
-                                    <TableCell>{index}</TableCell>
-                                    <TableCell component="th" scope="row">
-                                        {title}
-                                    </TableCell>
-                                    <TableCell align="right">{passion}</TableCell>
-                                    <TableCell align="right">{year}</TableCell>
-                                </TableRow>
-                            })}
+                            {renderHobbies()}
                         </TableBody>
                     </Table>
                 </TableContainer> :
